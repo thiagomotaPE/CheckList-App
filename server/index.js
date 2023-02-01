f1a52c951a6342d8c426838a57c9b287bd9c9e43
mota <thiagomotape@hotmail.com.br>
2023-02-01 16:39:02 -0300

    Update index.js
    tentando estabelecer a conexão com o banco de dados

--- server/index.js
+++ server/index.js
@@ -2,17 +2,18 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 
-const db = mysql.createConnection({
-    host: 'localhost',
+const db = mysql.createPool({
+    host: '127.0.0.1',
     user: 'mota',
-    password: '12345678',
-    database: 'checklistdb'
+    password: 'mota123',
+    database: 'checklistdb',
+    port: '3306'
 })
 
 app.get("/", (req, res) => {
-    const sqlInsert = "INSERT INTO tasks (name_task, description_task) VALUES ('arrumar a casa', 'preciso arrumar a casa antes que minha mãe chegue');"
+    const sqlInsert = "INSERT INTO tasks (id_task, name_task, description_task) VALUES ('1', 'arrumar a casa', 'preciso arrumar a casa antes que minha mãe chegue');"
     db.query(sqlInsert, (err, result) => {
-        res.send('teste 7')
+        res.send('teste 14')
     })
 })
 
